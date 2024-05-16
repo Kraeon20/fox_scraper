e2c4a60167203191f4cfd3134c137b266d93bf71
Kraeon20 <williams.asante515@gmail.com>
2024-05-16 22:01:00 +0000

    gunicorn added in requirements

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ def extract_social_media_links(page):
 
 def main(search_term, quantity):
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
 
         page.goto("https://www.google.com/maps", timeout=6000)
