e8710bf80a3fdf5005724dc34378b90e75988d78
Kraeon20 <willaims.asante515@gmail.com>
2024-08-21 16:33:06 +0100

    website opens in same browser window

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def extract_social_media_links(page):
 
 def main(search_term, quantity=9999999):
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
 
         print("Navigating to Google Maps...")
